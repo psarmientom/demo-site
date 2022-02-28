f16d86fccc6a903af21f933325ce65b7fc8873a3
psarmientom <psarmientom>
2022-02-28 15:03:38 -0500

    updated media query

---
+++ assets/js/test.js
@@ -0,0 +1,7 @@
+// alert("help")
+console.log("Working?")
+
+// this is a cooment
+
+/* Magna pars studiorum, prodita quaerimus. Vivamus sagittis lacus vel augue laoreet rutrum faucibus. At nos hinc posthac, sitientis piros Afros. Me non paenitet nullum festiviorem excogitasse ad hoc.
+Nihil hic munitissimus habendi senatus locus, nihil horum? Idque Caesaris facere voluntate liceret: sese habere. Quae vero auctorem tractata ab fiducia dicuntur. Gallia est omnis divisa in partes tres, quarum. Phasellus laoreet lorem vel dolor tempus vehicula. Ut enim ad minim veniam, quis nostrud exercitation. */
---
+++ js/test.js
@@ -0,0 +1,37 @@
+// alert("It's working")
+console.log("This is a string")
+
+console.log(4 + 7)
+console.log(16 % 3)
+console.log(+"a" + 8)
+
+console.log(10>9)
+
+const myVariable = "This is my first Variable"
+
+const myNumber = 1 
+
+console.log("4" +3)
+console.log ("This is the number" + " " + "8")
+
+var thisVar = 8
+var thisVar = 10
+
+let otherVar = 2
+otherVar = 4
+
+const lastVar = 7
+const otheOne = 2
+
+const firstOne = 1,
+secondOne = 2,
+thirdOne = 3
+
+const year = 2022
+console.log("This is the year " + year)
+console.log(typeof year)
+
+console.log(lastVar + otheOne)
+
+
+
